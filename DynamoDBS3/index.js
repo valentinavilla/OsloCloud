8065b5d1440811fc7112f095d5711628f78893fa
CristianoGambirasio <c.gambirasio6@studenti.unibg.it>
2022-04-29 16:23:39 +0200

    Correzioni

--- DynamoDBS3/index.js
+++ DynamoDBS3/index.js
@@ -1,32 +1,52 @@
-const AWS = require('aws-sdk')
-const DB = AWS.DynamoDB.DocumentClient;
-const S3 = new AWS.S3
+const AWS = require('aws-sdk');
+const parser = require('xml2js');
+const DB = new AWS.DynamoDB();
+const S3 = new AWS.S3;
 exports.handler = async (event) => {
-    
-    const bucket=event.Records[0].s3.bucket.name
-    const keyRaw=JSON.stringify(event.Records[0].s3.object.key)
+    //Estrazione nome bucket
+    const bucket = event.Records[0].s3.bucket.name;
 
-    const key_string= keyRaw.replace("+", " ");
-    const key=JSON.parse(key_string);
-    
+    //Estrazione key
+    const keyRaw = JSON.stringify(event.Records[0].s3.object.key);
+    const key_string = keyRaw.replace("+", " ");
+    const key = JSON.parse(key_string);
+
     //Get XML
-    const params={
+    const params = {
         Bucket: bucket,
         Key: key
-    }
+    };
     const data = await S3.getObject(params).promise();
-    const data_xml = data.Body.toString('utf-8')
-    
+    const data_xml = data.Body.toString('utf-8');
+
     //Parse JSON
     const data_string = await parser.parseStringPromise(data_xml).then(function (result) {
         return JSON.stringify(result);
     })
-    .catch(function (err) {
-        throw err;
-    });
+        .catch(function (err) {
+            throw err;
+        });
+    const data_json = JSON.parse(data_string);
 
-    const data_json= JSON.parse(data_string)
-    
-    console.log(data_xml)
-    
+    //Creazione item DynamoDB
+    const nomeGara = "" + data_json.ResultList.Event[0].Name;
+    const dataGara = "" + data_json.ResultList.Event[0].StartTime[0].Date;
+    const OraInizioGara = "" + data_json.ResultList.Event[0].StartTime[0].Time;
+    const OraFineGara = "" + data_json.ResultList.Event[0].EndTime[0].Time;
+
+    var DynamoParams = {
+        "TableName": 'Gare',
+
+        Item: {
+            "Nome": { S: nomeGara },
+            "Data": { S: dataGara },
+            "OraInizio": { S: OraInizioGara },
+            "OraFine": { S: OraFineGara }
+        }
+    };
+
+    //Inserimento in DynamoDB
+    DB.putItem(DynamoParams, function (err, data) {
+        if (err) throw err;
+    });
 };
--- SoftwareSimulazioneGare/index.js
+++ SoftwareSimulazioneGare/index.js
@@ -1,47 +1,56 @@
-const fs = require('fs')
-const xml2js = require('xml2js')
-const moment = require ('moment')
+const fs = require('fs')//Import FileSystem (input/output da file locali)
+const xml2js = require('xml2js')//Import Parser
+const moment = require ('moment')//Import moment (Gestione date)
 
 const parser = new xml2js.Parser();
 
-const inputFile = process.argv[2]
-const minPassati = process.argv[3]
+const inputFile = process.argv[2]//nome del file passato all'invocazione codice
+const minPassati = process.argv[3]//minuti trascorsi da inizio gara passato all'invocazione codice
 
 
 SimulaXML(inputFile,minPassati);
 
 async function SimulaXML (file,minutes) {
-    const file_xml = fs.readFileSync(file, 'utf8');
+    const file_xml = fs.readFileSync(file, 'utf8');//Lettura file 
 
+    //Parse xml->JSON
     const file_string = await parser.parseStringPromise(file_xml).then(function (result) {
         return JSON.stringify(result);
     });
     const file_json = JSON.parse(file_string);
 
+    //Estrazione inizio evento
     startTime = file_json.ResultList.Event[0].StartTime[0].Time
     startDate = file_json.ResultList.Event[0].StartTime[0].Date
     const start_string = startDate + " " + startTime;
-    const start = moment(start_string)
+    const start = moment(start_string)//Inizio evento come data
 
-    const now = start.add(minutes, 'm');
+    const now = start.add(minutes, 'm');//data di inizio + minuti passati
 
-    var output_json = file_json;
+    var output_json = file_json;//File di output
 
-    output_json.ResultList.ClassResult.forEach(function (categoria) {
+    //Modifica del file di output
+    output_json.ResultList.ClassResult.forEach(function (categoria) {//Per ogni categoria
         var i;
-        for (i = categoria.PersonResult.length - 1; i >= 0; i -= 1) {
-            if (!categoria.PersonResult[i].Result[0].FinishTime) {
+        for (i = categoria.PersonResult.length - 1; i >= 0; i -= 1) {//Ogni concorrente
+            if (!categoria.PersonResult[i].Result[0].FinishTime) {//Se non c'è il valore di arrivo
                 //Eliminare
                 continue;
             }
 
-            var finish = moment("" + categoria.PersonResult[i].Result[0].FinishTime)
+            var finish = moment("" + categoria.PersonResult[i].Result[0].FinishTime)//Data dell'arrivo del partecipante
 
-            if (finish.isAfter(now)) {
+            if (finish.isAfter(now)) {//Se l'arrivo è dopo il momento selezionato in input
                 //Eliminare
 
             }
         }
     })
+
+    //Parse output in xml
+
+
+    //Salva output 
+
 }
 

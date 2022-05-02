96119ca0d8a3fb094f483e26a7cbe8baa4e3004a
CristianoGambirasio <c.gambirasio6@studenti.unibg.it>
2022-05-02 17:48:50 +0200

    Fine software simulazione

--- SoftwareSimulazioneGare/index.js
+++ SoftwareSimulazioneGare/index.js
@@ -34,22 +34,42 @@ async function SimulaXML (file,minutes) {
         var i;
         for (i = categoria.PersonResult.length - 1; i >= 0; i -= 1) {//Ogni concorrente
             if (!categoria.PersonResult[i].Result[0].FinishTime) {//Se non c'è il valore di arrivo
-                //Eliminare
                 continue;
             }
 
             var finish = moment("" + categoria.PersonResult[i].Result[0].FinishTime)//Data dell'arrivo del partecipante
 
             if (finish.isAfter(now)) {//Se l'arrivo è dopo il momento selezionato in input
-                //Eliminare
+                delete categoria.PersonResult[i].Result[0].FinishTime;
+                delete categoria.PersonResult[i].Result[0].Time;
+                delete categoria.PersonResult[i].Result[0].TimeBehind;
+                delete categoria.PersonResult[i].Result[0].Position;
+                delete categoria.PersonResult[i].Result[0].Status;
+
+                var j = 0;
+                categoria.PersonResult[i].Result[0].SplitTime.forEach(function (split) {
+                    j++;
+                    if (!split.Time) {
+                        
+                    }
+                    else if (moment("" + categoria.PersonResult[i].Result[0].StartTime).add(split.Time, 's').isAfter(now)) {
+                        categoria.PersonResult[i].Result[0].SplitTime.splice(j-1, categoria.PersonResult[i].Result[0].SplitTime.length - j+1);
+
+                    }
+                })
             }
         }
     })
 
-    //Parse output in xml
-    //var output_xml = 
+    //Parse da JSON a XML
+    var builder = new xml2js.Builder();
+    var xml_output = builder.buildObject(output_json);
 
-    //Salva output 
+    //Stampa file output
+    fs.writeFile('output.xml', xml_output, function (err) {
+        if (err) throw err;
+        console.log('File creato con successo :D');
+    });
 
 }
 

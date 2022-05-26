6a712cb84053b59b142188e18b6c17f3ee334490
LorenzoFagiani <l.fagiani1@studenti.unibg.it>
2022-05-26 15:04:58 +0200

    updates

--- Flutter/oriresults/lib/Route/classesRoute.dart
+++ Flutter/oriresults/lib/Route/classesRoute.dart
@@ -55,7 +55,7 @@ class ClassesRouteState extends State<ClassesRoute> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Categorie'),
+        title: const Text('Classes'),
         backgroundColor: Color.fromARGB(255, 97, 206, 100),
         centerTitle: true,
       ),
